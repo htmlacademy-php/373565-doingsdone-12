02ec8301740d7fed7e97ede9ceadc2d3ea9ceeca
ИРИНА <8129p@mail.ru>
2020-04-04 17:04:45 +0300

    Переименовала поля

--- schema.sql
+++ schema.sql
@@ -6,7 +6,7 @@ USE doingsdone;
 
 CREATE TABLE users (
     id INT unsigned AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    datetime TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     email VARCHAR(128) UNIQUE NOT NULL,
     name VARCHAR(255) NOT NULL,
     password VARCHAR(64) NOT NULL
@@ -20,15 +20,15 @@ CREATE TABLE projects (
 
 CREATE TABLE tasks (
     id INT unsigned AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    datetime TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     status TINYINT(1) DEFAULT 0 NOT NULL,
     name VARCHAR(255) NOT NULL,
     file_path VARCHAR(255) NULL,
-    date TIMESTAMP NULL,
+    due_date TIMESTAMP NULL,
     user_id INT unsigned NOT NULL,
     project_id INT unsigned NOT NULL
 );
 
 CREATE INDEX status ON tasks(status);
 CREATE INDEX name ON tasks(name);
-CREATE INDEX date ON tasks(date);
+CREATE INDEX date ON tasks(due_date);
